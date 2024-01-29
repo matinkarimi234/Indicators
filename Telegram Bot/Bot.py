c664400b497a38c707576aeb7f634e040892716f
Matin <Matin@LAPTOP-23VIAVEQ>
2024-01-29 16:05:08 +0330

    Commit

--- Telegram Bot/Bot.py
+++ Telegram Bot/Bot.py
@@ -1,10 +1,12 @@
 import logging
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, Update
 from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes
 
 import finpy_tse as fpy
 import pandas as pd
+import jdatetime
+from pathlib import Path
 
 from Functions import *
 
@@ -24,13 +26,33 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Sends a message with three inline buttons attached."""
     keyboard = [
         [
-            InlineKeyboardButton("1 تثبیت روند صعودی", callback_data="ascending1"),
-            InlineKeyboardButton("چکش", callback_data="hammer"),
+            InlineKeyboardButton("1 تثبیت روند صعودی", callback_data="ascending1")
+        ],
+        [
+            InlineKeyboardButton("چکش", callback_data="hammer")
+            
+        ],
+        [
             InlineKeyboardButton("روند صعودی 2", callback_data="ascending2")
         ],
         [
-            InlineKeyboardButton("روند صعودی 1 و چکش", callback_data="hammer_ascend1"),
+            InlineKeyboardButton("روند صعودی 1 و چکش", callback_data="hammer_ascend1")
+        ],
+        [
             InlineKeyboardButton("مقایسه سقف با سال 1399", callback_data="compare_99")
+        ],
+
+        [
+            InlineKeyboardButton("روند صعودی 2 و چکش", callback_data="hammer_ascend2")
+        ],
+        [
+            InlineKeyboardButton("مقایسه کف ماهانه و 99", callback_data="min_month_99")
+        ],
+        [
+            InlineKeyboardButton("ایچیموکو کف و 99", callback_data="ichi_99")
+        ],
+        [
+            InlineKeyboardButton("کف 3 ماهه و آستانه خرید", callback_data="Buy_Min")
         ],
     ]
 
@@ -45,14 +67,15 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     
     dft = pd.DataFrame()
-
     # CallbackQueries need to be answered, even if no notification to the user is needed
     # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     await query.answer()
     response = ''
+
+    name_list_ichi = []
     if query.data == "ascending1":
         dft = ascending1()
-        for i in list(dft.index.values):
+        for i in dft['Ticker'].tolist():
             response += '--\t'+str(i)+'\t-- \n'
         await query.edit_message_text('\n نتیجه فیلتر صعودی یک' + response)
 
@@ -60,7 +83,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     elif query.data == 'hammer':
         dft = hammer()
-        for i in list(dft.index.values):
+        for i in dft['Ticker'].tolist():
             response += '--\t'+str(i)+'\t-- \n'
         await query.edit_message_text('\n نتیجه فیلتر چکش'+ response)
 
@@ -68,17 +91,23 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     elif query.data == "ascending2":
         dft = ascending2()
-        for i in list(dft.index.values):
+        for i in dft['Ticker'].tolist():
             response += '--\t'+str(i)+'\t-- \n'
         await query.edit_message_text('\n نتیجه فیلتر صعودی دو' + response)
 
         
 
     elif query.data == "hammer_ascend1":
-        dft = ascending2()
-        for i in list(dft.index.values):
+        dft = hammer_ascend1()
+        for i in dft['Ticker'].tolist():
             response += '--\t'+str(i)+'\t-- \n'
         await query.edit_message_text('\n نتیجه فیلتر صعودی 1 و چکش' + response)
+    
+    elif query.data == "hammer_ascend2":
+        dft = hammer_ascend2()
+        for i in dft['Ticker'].tolist():
+            response += '--\t'+str(i)+'\t-- \n'
+        await query.edit_message_text('\n نتیجه فیلتر صعودی 2 و چکش' + response)
 
 
     elif query.data == "compare_99":
@@ -87,6 +116,43 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             response += '--\t'+str(i)+'\t-- \n'
         await query.edit_message_text('\n نتیجه فیلتر مقایسه سقف با 99' + response)
 
+
+    elif query.data == "min_month_99":
+        dft = Min_Month()
+        for index,row in dft.iterrows():
+            date = str(row['Date']).split('-')
+            j_date = jdatetime.date.fromgregorian(day=int(date[2]),month=int(date[1]),year=int(date[0]))
+            response += '--\t'+str(row['Ticker'])+'  '+str(j_date.strftime("%Y-%m-%d"))+'  \t-- \n'
+            name_list_ichi.append(index)
+        await query.edit_message_text('\n مقایسه کف ماهیانه و سقف 99' + response)
+
+    elif query.data == "ichi_99":
+        with open('names.txt','r',encoding='utf-8',errors = 'ignore') as f:
+            name_list_ichi = f.readlines()
+        print(name_list_ichi)
+        photos = []
+        count = 0
+        num = str(len(name_list_ichi))
+        for name in name_list_ichi:
+            try:
+                ticker, filename = name.split(',')
+                Ichimoku(ticker.strip(),filename.strip())
+            except Exception as e :
+                print(e)
+            finally:
+                print(str(count)+'/'+num)
+                count += 1
+
+        data_folder = Path(r"/output/")
+        for file in os.listdir('output'):
+            photos.append(InputMediaPhoto(open(data_folder / file.strip()), 'rb'))
+        
+        await context.bot.send_media_group(chat_id=update.effective_chat.id, media=photos)
+    elif query.data == "Buy_Min":
+        dft = Min_Buy()
+        for i in dft['Ticker'].tolist():
+            response += '--\t'+str(i)+'\t-- \n'
+        await query.edit_message_text('\n نتیجه کف 3 ماهه و فیلتر آستانه خرید' + response)
 
 
 def main() -> None:
--- Telegram Bot/Functions.py
+++ Telegram Bot/Functions.py
@@ -1,8 +1,40 @@
+from datetime import timedelta , datetime
 import numpy as np
 import pandas as pd
 import os
 import jdatetime as jd
 import finpy_tse as fpy
+import mplfinance as mpf
+import uuid
+
+binance_dark = {
+    "base_mpl_style": "dark_background",
+    "marketcolors": {
+        "candle": {"up": "#3dc985", "down": "#ef4f60"},  
+        "edge": {"up": "#3dc985", "down": "#ef4f60"},  
+        "wick": {"up": "#3dc985", "down": "#ef4f60"},  
+        "ohlc": {"up": "green", "down": "red"},
+        "volume": {"up": "#247252", "down": "#82333f"},  
+        "vcedge": {"up": "green", "down": "red"},  
+        "vcdopcod": False,
+        "alpha": 1,
+    },
+    "mavcolors": ("#ad7739", "#a63ab2", "#62b8ba"),
+    "facecolor": "#1b1f24",
+    "gridcolor": "#2c2e31",
+    "gridstyle": "--",
+    "y_on_right": True,
+    "rc": {
+        "axes.grid": True,
+        "axes.grid.axis": "y",
+        "axes.edgecolor": "#474d56",
+        "axes.titlecolor": "red",
+        "figure.facecolor": "#161a1e",
+        "figure.titlesize": "x-large",
+        "figure.titleweight": "semibold",
+    },
+    "base_mpf_style": "binance-dark",
+}
 
 
 # MarketWatch Filters
@@ -20,7 +52,7 @@ def GetMarketWatch():
 
 def ascending1(dft = GetMarketWatch()):
     tl = (dft['Day_UL'] - dft['Day_LL'])
-    pl = (dft['High'] - dft['Low'])
+    pl = dft['Close']
     cl = (pl - dft['Open'])
     dft = dft.loc[(dft['Close'] > dft['Open']) & (((cl/tl) >= 0.7) | ((pl/tl) >= 0.6))]
     return dft
@@ -63,30 +95,47 @@ def hammer(dft = GetMarketWatch()):
     return dft
 
 
+
+def P_E(dft = GetMarketWatch()):
+    names = []
+    for index, row in dft.iterrows():
+        if np.float64(row['EPS']) != 0:
+            p_e = np.float64(row['Close']) / np.float64(row['EPS'])
+            if p_e <= 5:
+                names.append(row['Ticker'])
+    return names
+
 def hammer_ascend1():
 
     df1 = ascending1()
+    
+    # df_final = pd.DataFrame()
+    df_final = hammer(df1)
+
+    return df_final
+
+def hammer_ascend2():
+    
+    df1 = ascending2()
     
     df_final = pd.DataFrame()
     df_final = hammer(df1)
 
     return df_final
 
-
-
 # Chart Filters
 
-
-def get_max_year(name):
+def get_max_year(name,start = '1402-01-01'):
     today = jd.date.today().strftime("%Y-%m-%d")
     data = fpy.Get_Price_History(stock = name,
-                                 start_date = '1402-01-01',
+                                 start_date = start,
                                  end_date = today,
                                  ignore_date = False,
                                  adjust_price = True,
                                  show_weekday = False,
                                  double_date = False)
     maximum = max(data['Adj Close'])
+    data = data.drop(columns=['Open','High','Low','Close','Final','Volume','Value','No','Name','Market','Adj Open','Adj High','Adj Low','Adj Final'])
     return data[data['Adj Close'] == maximum]
 
 def Get_Today_From_99():
@@ -101,6 +150,7 @@ def Get_Today_From_99():
         for index, row in max_99.iterrows():
             try:
                 max_year = pd.concat([max_year,get_max_year(row['Ticker'])], ignore_index = True)
+                print(str(count)+' / '+ str(len(max_99.axes[0])))
                 count+=1
             except:
                 count+=1
@@ -116,7 +166,7 @@ def Compare_To_99():
         try:
             close_99 = np.int64(row['Adj Close'])
             close_year = np.array(max_year[max_year['Ticker'] == row['Ticker']]['Adj Close'])
-            if ((close_99 * 2) + close_99) <= close_year[0] :
+            if ((close_99 * 1.5) + close_99) <= close_year[0] :
                 result.append(row['Ticker'])
         except Exception as error:
             pass
@@ -124,3 +174,199 @@ def Compare_To_99():
     res = []
     [res.append(i) for i in result if i not in res]
     return res
+
+def Min_Last_Month(name):
+
+    start = (jd.date.today() - timedelta(days=90)).strftime("%Y-%m-%d")
+    today = jd.date.today().strftime("%Y-%m-%d")
+    today_M = datetime.now().strftime("%Y-%m-%d")
+    data = fpy.Get_Price_History(stock = name,
+                                 start_date = start,
+                                 end_date = today,
+                                 ignore_date = False,
+                                 adjust_price = True,
+                                 show_weekday = False,
+                                 double_date = True)
+    minimum = min(data['Adj Close'])
+    return data[data['Adj Close'] == minimum]
+
+def Min_Month():
+    today = jd.date.today().strftime("%Y-%m-%d")
+    filename = 'Month-'+ today + '.csv'
+    dft = pd.DataFrame()
+    if os.path.isfile(filename):
+        dft = pd.read_csv(filename)
+    else:
+        print('Getting Last 3 Months Min...')
+        names_99 = Compare_To_99()
+        print(names_99)
+        names_pe = P_E()
+        print('\n')
+        print(names_pe)
+        print('\n')
+        names = list(set(names_99) & set(names_pe))
+        print(names)
+        count = 0
+        with open('names.txt','wb') as f:
+            for name in names:
+                path = str(uuid.uuid4())
+                f.write((name+','+path[0:7]+ '\n' ).encode('utf-8','ignore'))
+        for name in names:
+            dft = pd.concat([dft,Min_Last_Month(name)], ignore_index = True)
+            print(str(count)+' / '+ str(len(names)))
+            count +=1
+        dft.to_csv(filename)
+    return dft
+
+def Get_Each_Data(name,start,end = jd.date.today().strftime("%Y-%m-%d")):
+    data = pd.DataFrame()
+    data = fpy.Get_Price_History(stock = name,
+                                 start_date = start,
+                                 end_date = end,
+                                 ignore_date = False,
+                                 adjust_price = True,
+                                 show_weekday = False,
+                                 double_date = True)
+    return data
+
+
+def color(goldencrossover):
+    UP = []
+    DOWN = []
+    for i in range(len(goldencrossover)):
+        if goldencrossover['Tenkan-sen'][i] < goldencrossover['Kijun-sen'][i]:
+            UP.append(float(goldencrossover['Tenkan-sen'][i]))
+            DOWN.append(np.nan)
+        elif goldencrossover['Tenkan-sen'][i] > goldencrossover['Kijun-sen'][i]:
+            DOWN.append(float(goldencrossover['Tenkan-sen'][i]))
+            UP.append(np.nan)
+        else:
+            UP.append(np.nan)
+            DOWN.append(np.nan)
+    goldencrossover['up'] = UP
+    goldencrossover['down'] = DOWN
+    return goldencrossover
+
+def golden_cal(df):
+    goldenSignal = []
+    deathSignal = []
+    position = False
+    for i in range(len(df)):
+        if df['Kijun-sen'][i] > df['Tenkan-sen'][i] and 0.95 < (float(df['Kijun-sen'][i-13])/float(df['Kijun-sen'][i])) < 1.05: # Kijun sen Must be a straight line
+            if position == False :
+                goldenSignal.append((df['Tenkan-sen'][i]-df['Tenkan-sen'][i]*0.01))
+                deathSignal.append(np.nan)
+                position = True
+            else:
+                goldenSignal.append(np.nan)
+                deathSignal.append(np.nan)
+        elif df['Kijun-sen'][i] < df['Tenkan-sen'][i] and 0.95< (float(df['Tenkan-sen'][i-13])/float(df['Tenkan-sen'][i])) < 1.05: # Tenkan sen Must be a straight line
+            if position == True:
+                goldenSignal.append(np.nan)
+                deathSignal.append((df['Tenkan-sen'][i]+df['Tenkan-sen'][i]*0.01))
+                position = False
+            else:
+                goldenSignal.append(np.nan)
+                deathSignal.append(np.nan)
+        else:
+            goldenSignal.append(np.nan)
+            deathSignal.append(np.nan)
+    df['GoldenCrossOver'] = goldenSignal
+    df['DeathCrossOver'] = deathSignal
+
+
+def Ichimoku(name,filename):
+    start = (jd.date.today() - timedelta(days=800)).strftime("%Y-%m-%d")
+    today = jd.date.today().strftime("%Y-%m-%d")
+    path = "output\\"+filename+'_'+today+".jpg"
+    if os.path.isfile(path):
+        pass
+    else:
+        df = Get_Each_Data(name,start)
+        df.reset_index(drop=True,inplace=True)
+        df.set_index('Date',inplace=True)
+
+
+        # Formula To Obtain Ichimoku Cloud 
+        df['Tenkan-sen'] = (df['High'].rolling(window=9).max() + df['Low'].rolling(window=9).min()) / 2
+        df['Kijun-sen'] = (df['High'].rolling(window=26).max() + df['Low'].rolling(window=26).min()) / 2 
+        df['Senkou_Span_A'] = (df['Tenkan-sen'] + df['Kijun-sen']) / 2 
+        df['Senkou_Span_B'] = (df['High'].rolling(window=52).max() + df['Low'].rolling(window=52).min()) / 2 
+        df['Chikou_Span'] = df['Close'].shift(periods=-26) 
+
+        a = df[['Tenkan-sen']]
+        b = df[['Kijun-sen']]
+        c = df[['Chikou_Span']]
+        d = df[['Senkou_Span_A']]
+        e = df[['Senkou_Span_B']]
+
+        golden_cal(df)
+
+        #Fuction Color Applied And Df Generated 
+        goldencrossover = color(df)
+
+        up_sma100 = goldencrossover[['up']]
+        down_sma100 = goldencrossover[['down']]
+        up_sma21 = goldencrossover[['Kijun-sen']]
+        gco = goldencrossover[['GoldenCrossOver']]
+        dco = goldencrossover[['DeathCrossOver']]
+
+
+        ic = [
+        mpf.make_addplot(a,color='#469ff2',alpha=0.5,label='Tenkan-sen'),
+        mpf.make_addplot(b,color='#f26bca',alpha=0.5,label='Kijun-sen'),
+        mpf.make_addplot(c,color='#99ff59',alpha=0.8,label='Chikou_Span'),
+        mpf.make_addplot(d,color='#006B3D',alpha=0.8,label='Senkou_Span_A'),
+        mpf.make_addplot(e,color='#D3212C',alpha=0.8,label='Senkou_Span_B'),
+        mpf.make_addplot(gco,type='scatter',markersize=200,marker='v',color='red',panel=0),
+        mpf.make_addplot(dco,type='scatter',markersize=200,marker='^',color='green',panel=0),
+        ]
+
+        ichimoko_fill_up = dict(y1 = df['Senkou_Span_A'].values, y2 = df['Senkou_Span_B'].values, where = df['Senkou_Span_A'] >= df['Senkou_Span_B'], alpha = 0.5, color = '#a6f7a6')
+        ichimoko_fill_down = dict(y1 = df['Senkou_Span_A'].values, y2 = df['Senkou_Span_B'].values, where = df['Senkou_Span_A'] < df['Senkou_Span_B'], alpha = 0.5, color = '#FC8EAC')
+
+        
+        mpf.plot(
+        df,
+        volume=True,
+        type="candle",
+        title = name,
+        fill_between = [ichimoko_fill_up,ichimoko_fill_down],
+        style= 'yahoo',
+        addplot=ic,
+        figsize=(60,30),
+        savefig=dict(fname=path,dpi=130,pad_inches=0.25)
+        )
+
+
+def Buy_Queue(dft = GetMarketWatch()):
+    pl = dft['Close']
+    tmax = dft['Day_UL']
+
+    dft = dft.loc[(pl<tmax) & (pl>=((tmax/100)*99))]
+
+    return dft
+
+def Min_Buy():
+    today = jd.date.today().strftime("%Y-%m-%d")
+    filename = 'Month-Buy-'+ today + '.csv'
+    dft = pd.DataFrame()
+    if os.path.isfile(filename):
+        dft = pd.read_csv(filename)
+    else:
+        Buy = Buy_Queue()
+        count = 0
+        for index, row in Buy.iterrows():
+            name = row['Ticker']
+            print(str(count) + '/'+str(len(Buy)))
+            try:
+                minimum = Min_Last_Month(name)
+                min_price = np.int64(minimum['Close'])
+            
+                if min_price < np.int64(row['Close'])< (min_price * 1.1):
+                    dft = pd.concat([dft,minimum], ignore_index = True)
+                    print('OK!')
+            except Exception as error:
+                pass
+            count+=1
+    return dft
